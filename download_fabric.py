59b8bcf47ff034ecfebf55351e67313fd8eb9d2d
DaZiDian <113085496+DaZiDian@users.noreply.github.com>
2024-02-24 07:57:14 +0800

    Update download_fabric.py

--- download_fabric.py
+++ download_fabric.py
@@ -3,8 +3,8 @@ import requests
 import hashlib
 
 # 创建目录
-if not os.path.exists('versions/fabric'):
-    os.makedirs('versions/fabric')
+if not os.path.exists('versions/Fabric'):
+    os.makedirs('versions/Fabric')
 
 # 获取版本信息
 response = requests.get("https://meta.fabricmc.net/v2/versions")
@@ -17,20 +17,23 @@ if 'game' not in data:
 
 # 遍历每个游戏版本
 for game_version in data['game']:
-    print(f"正在处理游戏版本: {game_version['version']}")
-    # 检查'loader'键是否存在
-    if 'loader' not in game_version:
-        print(f"在游戏版本 {game_version['version']} 中没有找到任何加载器版本。响应数据: {game_version}")
-        continue
+    game_version_number = game_version['version']
+    print(f"正在处理游戏版本: {game_version_number}")
+
+    # 获取每个游戏版本的加载器版本信息
+    loader_response = requests.get(f"https://meta.fabricmc.net/v2/versions/loader/{game_version_number}")
+    loader_data = loader_response.json()
 
     # 遍历每个加载器版本
-    for loader_version in game_version['loader']:
-        print(f"正在处理加载器版本: {loader_version['version']}")
+    for loader_version in loader_data:
+        loader_version_number = loader_version['loader']['version']
+        print(f"正在处理加载器版本: {loader_version_number}")
+
         # 获取下载信息
-        download_url = f"https://meta.fabricmc.net/v2/versions/loader/{game_version['version']}/{loader_version['version']}/1.0.0/server/jar"
+        download_url = f"https://meta.fabricmc.net/v2/versions/loader/{game_version_number}/{loader_version_number}/server/jar"
         print(f"正在从以下地址下载: {download_url}")
         file_response = requests.get(download_url)
-        file_path = f"versions/fabric/{game_version['version']}/{loader_version['version']}/server.jar"
+        file_path = f"versions/fabric/{game_version_number}/{loader_version_number}/server.jar"
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'wb') as f:
             f.write(file_response.content)
